1f704c53c2e8bac525b7da2b57ed24b8f4497b05
JorgeMarrian-7484 <jorgemarrianduran@gmail.com>
2025-11-12 22:55:40 -0300

    Subida de login y upt de vies

--- medicWeb/urls.py
+++ medicWeb/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from appMedic.views import inicio,login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',inicio, name='inicio'),
+    path('IniciarSesion/',login,name='login')
 ]
